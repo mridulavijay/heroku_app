65710a1c8b60bd5623c8be25849a5331e0d00637
mridulavijay <mridula.reghu@gmail.com>
2022-08-26 23:14:12 +0530

    new1

--- app.js
+++ app.js
@@ -32,4 +32,6 @@ app.post('/api/newdata',function(req,res){
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
    });
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });   
